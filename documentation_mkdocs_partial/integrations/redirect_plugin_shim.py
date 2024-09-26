366ed3a6f18155b8dc32f7db9228794b67988631
Konstantin Nosov <konstantin.nosov@gmail.com>
2024-09-26 13:37:51 +0300

    hide stub pages for redirected urls from navigation

--- documentation_mkdocs_partial/integrations/redirect_plugin_shim.py
+++ documentation_mkdocs_partial/integrations/redirect_plugin_shim.py
@@ -1,7 +1,7 @@
 import frontmatter
 from mkdocs import plugins
 from mkdocs.config.defaults import MkDocsConfig
-from mkdocs.structure.files import File, Files
+from mkdocs.structure.files import File, Files, InclusionLevel
 from mkdocs_redirects.plugin import RedirectPlugin  # pylint: disable=import-error
 
 
@@ -14,7 +14,10 @@ class RedirectPluginShim(RedirectPlugin):
     def add_redirects(self, files: Files, file, redirect_from: list[str], config: MkDocsConfig):
         for redirect in redirect_from:
             self.config.setdefault("redirect_maps", {})[redirect] = file.src_path.replace("\\", "/")
+            # Register stub page to avoid warnings about missing link targets
             stub = frontmatter.Post("Redirect")
             stub.metadata["layout"] = "redirect"
-            file = File.generated(config=config, src_uri=redirect, content=frontmatter.dumps(stub))
+            file = File.generated(
+                config=config, src_uri=redirect, content=frontmatter.dumps(stub), inclusion=InclusionLevel.EXCLUDED
+            )
             files.append(file)
